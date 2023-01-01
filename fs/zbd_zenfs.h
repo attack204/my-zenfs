7a7adc2a1947674f81bf256e30e1272f5e677701
attack204 <attack204@qq.com>
2023-01-01 17:18:50 +0800

    update:

--- fs/zbd_zenfs.h
+++ fs/zbd_zenfs.h
@@ -42,7 +42,7 @@ const int ENABLE_PRECOMPACTION = 0;
 const double GC_THRESHOLD = 1;
 const int ENABLE_CASE1 = 0;
 const int ENABLE_CASE2 = 0;
-const int ENABLE_LIMIT_LEVEL = 3;
+int ENABLE_LIMIT_LEVEL = 3;
 
 const int T = 50;
 const uint64_t GC_START_LEVEL = 60;
